cad5c8d025365ccac59997466a8c5ca295547606
Ari Pickar <ari.pickar@gmail.com>
2018-08-05 09:48:58 -0700

    added getArtistIdsFromNames

--- services/genreController.py
+++ services/genreController.py
@@ -5,18 +5,30 @@ def getGenres():
 	genres = ['classical', 'pop', 'jazz', 'rock']
 	return algorithm.get_random_genres(genres)
 
-def getArtists(genre):
+def getArtistsIds(genre):
 	conn = sqlite3.connect('genre.db')
 	c = conn.cursor()
 	c.execute("SELECT artistID, popularity FROM genre where (genre.genreName = ?)", (genre,))
 	x = c.fetchall()
 	temp = algorithm.normalize(x)
 	artistID1, artistID2 = algorithm.get_random_artists(temp[0],temp[1])
-	c.execute("select artistName from genre where (genre.artistId = ?)", (artistID1,))
+	return artistID1, artistID2
+
+def getArtistNames(artistId1, artistId2):
+	conn = sqlite3.connect('genre.db')
+	c = conn.cursor()
+	c.execute("SELECT artistName from genre where (genre.artistId = ?)", (artistId1,))
 	artist1 = c.fetchone()
-	c.execute("select artistName from genre where (genre.artistId = ?)", (artistID2,))
+	c.execute("SELECT artistName from genre where (genre.artistId = ?)", (artistId2,))
 	artist2 = c.fetchone()
 	return artist1,artist2
+
+def getArtistIdsFromNames(artistName1):
+	conn = sqlite3.connect('genre.db')
+	c = conn.cursor()
+	c.execute("SELECT artistId from genre where (genre.artistName = ?)", (artistName1,))
+	artist1 = c.fetchone()
+	return artist1
 
 def getSongs(artist, last10):
 	conn = sqlite3.connect('song.db')
@@ -32,6 +44,6 @@ def getSongs(artist, last10):
 	return song1, song2
 
 for i in range(10):
-	x = getArtists(getGenres()[0])[0]
-	print(x)
+	x, y = getArtistsIds(getGenres()[0])
+	print(getArtistNames(x, y))
 	print(getSongs(x , []))
